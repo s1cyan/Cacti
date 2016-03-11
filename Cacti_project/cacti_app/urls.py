59a49767f0108cb4ebab679308d1bf48695b87a4
s1cyan <claudiab.yan@gmail.com>
2016-03-11 09:54:39 -0500

    Linked templates to app. removed script lines from navbar.html bc its in navbar_jquery.html

--- Cacti_project/cacti_app/urls.py
+++ Cacti_project/cacti_app/urls.py
@@ -5,4 +5,12 @@ from . import views
 urlpatterns = [
     # TODO: Add urls
     url(r'^$', views.greetings, name='greetings'),
+    url(r'^friends', views.friends, name='friends'),
+    url(r'^home', views.home, name='home'),
+    url(r'^login', views.login, name='login'),
+    url(r'^set-your-schedule', views.post_registration, name='post-Registration'),
+    url(r'^profile', views.profile, name='profile'),
+    url(r'^registration', views.registration, name='registration'),
+    url(r'^search', views.search, name='search'),
+
 ]
--- Cacti_project/cacti_app/views.py
+++ Cacti_project/cacti_app/views.py
@@ -2,8 +2,45 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request): 
+
+
+def greetings(request):
     context_dict = {
             'page_title': 'Cacti'
             }
     return render(request, 'greetings.html', context_dict)
+
+
+def friends(request):
+    context_dict = {'page_title': 'Cacti: Friends'}
+    return render(request,'friends_page.html',context_dict)
+
+
+def home(request):
+    context_dict = {'page_title': 'Cacti: Home'}
+    return render(request, 'home-page.html',context_dict)
+
+
+def login(request):
+    context_dict = {'page_title': 'Cacti: Login'}
+    return render(request, 'login-page.html',context_dict)
+
+
+def post_registration(request):
+    context_dict = {'page_title': 'Setting up your schedule'}
+    return render(request, 'post-registration.html',context_dict)
+
+
+def profile(request):
+    context_dict = {'page_title': 'Cacti:Profile'}
+    return render(request, 'post-registration.html',context_dict)
+
+
+def registration(request):
+    context_dict = {'page_title': 'Register for Cacti'}
+    return render(request, 'registration.html',context_dict)
+
+
+def search(request):
+    context_dict = {'page_title': 'Cacti: Search for friends'}
+    return render(request, 'search-page.html',context_dict)
