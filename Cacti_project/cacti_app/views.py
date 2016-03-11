e96e54a3cde111daa959c9181f36ca8d5804a870
Porrith Suong <psuong95@gmail.com>
2016-03-11 00:44:41 -0500

    Moves the templates and static folders to the relative app to comply to Django 1.9 settings.

--- Cacti_project/cacti_app/views.py
+++ Cacti_project/cacti_app/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Some fking page")
+    context_dict = {
+            'page_title': 'Cacti'
+            }
+    return render(request, 'greetings.html', context_dict)
