f55a30c5473a18a3031c23a41fd50927c64d93a3
s1cyan <claudiab.yan@gmail.com>
2016-03-11 00:49:31 -0500

    trying to set up first template

--- Cacti_project/cacti_app/views.py
+++ Cacti_project/cacti_app/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Some fking page")
+
+def greetings(request):
+    return render(request, 'templates/greetings.html')
