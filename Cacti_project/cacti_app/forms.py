114272388ded6c44199590498fedf842a1a5a15b
Porrith Suong <psuong95@gmail.com>
2016-03-20 23:27:44 -0400

    Adds an association between DayForm and Day Model.

--- Cacti_project/cacti_app/forms.py
+++ Cacti_project/cacti_app/forms.py
@@ -26,4 +26,7 @@ class DayForm(forms.ModelForm):
     This form is associated with the Day Model found in 
     models.py.
     """
+    class Meta:
+        model = Day
+        field = ['date']
     pass
